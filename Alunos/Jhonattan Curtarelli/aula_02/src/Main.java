f09b6fe4fc519736d049cb7fcd7a99f447b7e17c
Jhonattan Curtarelli <Jhonattan.curtarelli@outlook.com>
2025-03-04 18:08:52 -0300

    feat: creating option to calculate change

--- Alunos/Jhonattan Curtarelli/aula_02/src/Main.java
+++ Alunos/Jhonattan Curtarelli/aula_02/src/Main.java
@@ -4,20 +4,27 @@ import java.util.Scanner;
 public class Main {
     public static void calculaPrecoTotal(Scanner sc)
     {
-        int quantidade = 0;
-        BigDecimal preco = new BigDecimal(0);
         System.out.println("Insira a quantidade da planta referida");
-        quantidade = sc.nextInt();
+        int quantidade = sc.nextInt();
         System.out.println("Insira o preço da planta referida");
-        preco = sc.nextBigDecimal();
+        BigDecimal preco = sc.nextBigDecimal();
         System.out.println("Quantidade: " + quantidade + " Preço: " + preco);
         System.out.println("O Preço total é: R$ " + preco.multiply(new BigDecimal(quantidade)));
+    }
+    public static void calculaTroco(Scanner sc)
+    {
+        System.out.println("Insira o valor recebido");
+        BigDecimal valorRecebido = sc.nextBigDecimal();
+        System.out.println("Insira o valor total");
+        BigDecimal valorTotal = sc.nextBigDecimal();
+        System.out.println("Valor Recebido: R$ " + valorRecebido + " Valor Total: R$ " + valorTotal);
+        System.out.println("O troco é: R$ " + valorRecebido.subtract(valorTotal));
     }
     public static void menu (Scanner sc)
     {
         System.out.println(
                 "------------------------ MENU ------------------------\n" +
-                "1 - Cálcular Preço Total\n" +
+                "1 - Calcular Preço Total\n" +
                 "2 - Calcular Troco\n" +
                 "3 - Sair\n"
         );
@@ -29,7 +36,8 @@ public class Main {
                 menu(sc);
                 break;
             case 2:
-                System.out.println("Cálcular Preço Total");
+                System.out.println("Calcular Preço Total");
+                calculaTroco(sc);
                 menu(sc);
                 break;
             case 3:
@@ -40,8 +48,6 @@ public class Main {
                 menu(sc);
                 break;
         }
-
-
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
